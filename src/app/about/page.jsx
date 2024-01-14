1c38cb09ff78f9675ea09965d72727a8c1b5a295
homermeng <homer.fansheng.meng@gmail.com>
2024-01-14 12:35:38 +0800

    Adding about and contact pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,35 @@ import styles from "./about.module.css"
 
 const AboutPage = () => {
     return (
-      <div>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <h2 className={styles.subTitle}>About pmAGI</h2>
+          <h1 className={styles.title}>We create AI tools and agents that will change the way Construction industry.</h1>
+          <p className={styles.description}>A 3D model design of the Empire State Building morphing into a basketball net, representing 
+            the New York Knicks. The building is detailed, with its iconic Art Deco architecture, and 
+            transitions smoothly into the orange and blue colors of the Knicks. The basketball net is made of silver 
+            metal, with a hoop that glows in the Knicks colors.
+          </p>
+          <div className={styles.boxes}>
+            <div className={styles.box}>
+              <h1>60+</h1>
+              <p>Years of experience</p>
+            </div>
+            <div className={styles.box}>
+              <h1>80+</h1>
+              <p>Countries of practice</p>
+            </div>
+            <div className={styles.box}>
+              <h1>40K+</h1>
+              <p>Users</p>
+            </div>
+          </div>
+        </div>
+        
+
+        
         <div className={styles.imgContainer}>
-          <Image src="/about.png" alt="" fill />
+          <Image src="/about.png" alt="" fill className={styles.img}/>
         </div>
       </div>
     )
